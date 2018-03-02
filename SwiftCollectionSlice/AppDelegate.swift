12c07e876dd7548fa05bc1c4a8d516466ab628a7
quasimodal <quasimodal@quasimodal.net>
2018-03-02 01:32:55 -0800

    Initial Commit

---
+++ SwiftCollectionSlice/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2018 Quincey Morris. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+	func applicationDidFinishLaunching (_ notification: Notification)
+	{
+	}
+	
+}
+
---
+++ SwiftCollectionSlice/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2018 Quincey Morris. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ViewController: NSViewController
+{
+	override func viewDidLoad ()
+	{
+		super.viewDidLoad ()
+	}
+	
+}
+
